a479f9035d60304fcf63683cb7949f357a076541
voltara13 <43500741+voltara13@users.noreply.github.com>
2021-10-11 12:36:58 +0700

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/turingMachine_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/turingMachine_autogen"
-  "turingMachine_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/turingMachine_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
